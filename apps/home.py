bbfe39e5090c0170326d4162cfdd5a301779956c
Edgar Gutierrez <edgargu47@gmail.com>
2021-03-13 19:45:38 -0600

    title

--- apps/home.py
+++ apps/home.py
@@ -14,7 +14,7 @@ layout = html.Div([
     dbc.Row(
         dbc.Col([
             html.Img(src='../assets/sanpedro.jpg', style={'max-width':'100%', 'height':'auto'}),
-             html.H2('Construyendo una movilidad segura, eficiente y sustentable',
+             html.H2('Centro de Gestión de Movilidad',
                 style={'position': 'absolute', 'top': '50%', 'left': '50%',
                 'transform': 'translate(-50%, -50%)','color': 'white','text-align':'center'})
         ])
