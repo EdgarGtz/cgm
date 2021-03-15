89227eb31d9b0064938d08eff857fb844e2b9014
Edgar Gutierrez <edgargu47@gmail.com>
2021-03-14 20:33:40 -0600

    .

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import dash_auth
 
 
 app = dash.Dash(__name__, title='Centro de Gestión de Movilidad',
-				external_stylesheets=[dbc.themes.BOOTSTRAP])
-				#meta_tags=[{'name': 'viewport',
-                             #'content': 'width=device-width, initial-scale=1.0'}])
+				external_stylesheets=[dbc.themes.BOOTSTRAP],
+				meta_tags=[{'name': 'viewport',
+                             'content': 'width=device-width, initial-scale=1.0'}])
 
 
 
