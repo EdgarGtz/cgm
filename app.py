a05e2a9b6b427a557472dbab69a2e49fa3cfde5d
Edgar Gutierrez <edgargu47@gmail.com>
2021-03-22 14:48:21 -0600

    os

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc 
 from dash.dependencies import Input, Output
 import dash_auth
+import os
 
 
 app = dash.Dash(__name__, title='Centro de Gestión de Movilidad',
